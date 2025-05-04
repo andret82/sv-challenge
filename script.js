8bce57a242bf2e52953a110da50f888f91bba454
Andre Tusch <andre.tusch@gmail.com>
2025-05-04 10:50:35 +0200

    update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
    * 1) ANIMIERTER DONUT-PROGRESS
    ****************************************************************************/
   // Your values
-  const current = 4816; // e.g., 5000
+  const current = 5784; // e.g., 5000
   const goal = 25000; // e.g., 25000
 
   // DOM elements
